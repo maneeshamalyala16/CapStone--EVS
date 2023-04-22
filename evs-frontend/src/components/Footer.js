3d8826c700f2581c2fb9d189fc01a216f5db31be
maneeshamalyala16 <131522247+maneeshamalyala16@users.noreply.github.com>
2023-04-22 14:51:34 +0530

    Add files via upload

---
+++ evs-frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export default function Footer() {
+  return (
+    <div className="foot">
+      <footer>&copy; copyrights..!! ELectronic Voting System</footer>
+    </div>
+  );
+}
---
+++ evs-frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+export default function Header() {
+  return (
+    <div className="head">
+      <div className="my-head-flex">
+        
+        <a href="http://localhost:3000/" className="my-head">
+          ELectronic Voting System
+        </a>
+      </div>
+    </div>
+  );
+}
